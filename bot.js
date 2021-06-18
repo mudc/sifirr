bd96127aad92577841df443ed054452f5f1ae3d4
mudc <48283064+mudc@users.noreply.github.com>
2021-06-19 01:47:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,20 +15,25 @@ function isNumeric(num) {
     return !isNaN(num)
 }
 
-// Belirli mesajlara göre yapılan işlemler burada
-bot.on('message', message => {
-
-    if (message.author.bot) return;    // Bot kendi yazdığı mesajları ignoreluyor.
-    if (message.channel.type === "dm") return; // Özel mesajları ignore
-    let args = message.content.substring(PREFIX.length).split(" ");
+// Eğer herhangi bir mesaj silinirse mesajla ilgili bilgileri ilgili kanala yazıyor
+bot.on('messageDelete', function (message, channel) {
 
     if (message.guild.id !== kabrockieServer) {
         return;
     }
 
+    bot.channels.get(deleteLogChannel).send("**[" + Date(Date.now()) + "]** " + message.channel + " kanalında *" + String(message.author.id) + " (" + message.author.username + ")* kişisinin mesajı silindi: " + '"' + message.content + '"');
 
-
-
+    // Eğer mesaj sadece bir fotoğraftan vs. oluşuyorsa boş mesaj olarak gözüküyor.
+    // Bu yüzden her attachmentın linkini log kanalına atıyor
+    // Silinen attachmentları görmenin yolu ne yazık ki yok ancak dosya adından ne olduğu bi ihtimal anlaşılabilir
+    if ((message.attachments).array().length > 0) {
+        var Attachment = (message.attachments).array();
+        Attachment.forEach(function (attachment) {
+            bot.channels.get(picLogChannel).send(attachment.url);
+        })
+    }
+});
 
 
 client.on('ready', () => {
