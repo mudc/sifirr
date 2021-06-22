21872a543bf3b0cbf29732bd2ca48e0876bef719
mudc <48283064+mudc@users.noreply.github.com>
2021-06-23 02:57:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,7 +26,7 @@ client.on('ready', () => {
 
 client.on('messageDelete', message => {
   console.log(`a message saying "${message.cleanContent}" was deleted from channel: ${message.channel.name} at ${new Date()}`);
-  client.channels.get("855551660175917116").send(`A message saying "***${message.cleanContent}***" + "***${message.author.id}***" + "***${message.author.username}***" has been deleted at ${currentDate.toLocaleString("tr-TR")}`)
+  client.channels.get("855551660175917116").send(`A message saying "***${message.cleanContent}***" + "***${message.author.id}***" + "***${message.author.username}***" has been deleted at ${myDate.toLocaleString("tr-TR")}`)
 });
 
 
