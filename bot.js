6d954d478be7a9358d4ab85d024e6d969b6a7de4
mudc <48283064+mudc@users.noreply.github.com>
2021-06-26 01:32:58 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -156,8 +156,10 @@ client.on('message', msg => {
 	if (msg.content.toLowerCase() === prefix + '321') {
 		msg.channel.send('https://media.giphy.com/media/d9wPasV7ukkta/giphy.gif');
 	}
-	if (msg.content.toLowerCase() === prefix + 'theend') {
-		msg.channel.send('https://tenor.com/view/lisk-the-end-gif-10529426');
+	if (msg.member.roles.find(r => r.name === "Mayor") || msg.member.roles.find(r => r.name === "Mods")) {
+		 if (msg.content.toLowerCase() === prefix + 'theend') {
+        	     msg.channel.send('https://tenor.com/view/lisk-the-end-gif-10529426%27); 
+		 }
 	}
 	if (msg.content.toLowerCase() === prefix + 'film') {
 		msg.channel.send('https://tenor.com/view/spongebob-square-pants-spongebob-patrick-squidward-popcorn-gif-3531993');
